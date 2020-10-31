567b98773e6af57fbe40f41ca9a46719d605f1a8
Frantisek <f.lucivjansky@gmail.com>
2020-10-31 10:15:11 +0100

    improve autocomplete and not found page

--- client/src/public/components/Places.tsx
+++ client/src/public/components/Places.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
@@ -23,11 +23,21 @@ interface PlacesProps {
 
 export function Places({ county, onChange, selected, label, size, className }: PlacesProps) {
   const { response, isLoading } = useCollectionPointsPublic(county);
+  const [searchValue, setSearchValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+  const [isOpen, setOpen] = useState(false);
   const classes = useStyles();
   const value =
     selected && response ? response?.find(it => String(it.id) === String(selected)) : null;
+
+  useEffect(() => {
+    if (value) {
+      setInputValue(getTextForSearchInput(value));
+    }
+  }, [value]);
   return (
     <Autocomplete
+      open={isOpen}
       autoHighlight
       className={className}
       size={size}
@@ -40,7 +50,24 @@ export function Places({ county, onChange, selected, label, size, className }: P
           onChange(value);
         }
       }}
-      getOptionLabel={option => `${option.city}, ${modifyPlace(option.address, option.city)}`}
+      inputValue={inputValue}
+      onInputChange={(evt, value, reason) => {
+        if (reason !== 'reset') {
+          setSearchValue(value);
+          setInputValue(value);
+        }
+      }}
+      onClose={() => {
+        if (value) {
+          setInputValue(getTextForSearchInput(value));
+        }
+        setOpen(false);
+      }}
+      onOpen={() => {
+        setInputValue(searchValue);
+        setOpen(true);
+      }}
+      getOptionLabel={option => getTextForSearchInput(option)}
       ListboxComponent={ListboxComponent as React.ComponentType<React.HTMLAttributes<HTMLElement>>}
       renderOption={option => (
         <div>
@@ -57,11 +84,16 @@ export function Places({ county, onChange, selected, label, size, className }: P
   );
 }
 
+function getTextForSearchInput(entity: CollectionPointEntity) {
+  return `${entity.city}, ${modifyPlace(entity.address, entity.city)}`;
+}
+
 function modifyPlace(place: string, city: string) {
-  return place
+  const res = place
     .replace(city + ' - ', '')
     .replace(city + ', ', '')
     .replace(city, '');
+  return res ? res : place || city;
 }
 
 const LISTBOX_PADDING = 8;
